4af7be7eb111bea983de263ff3f68218bab30cdb
liuzhuanghong <liuzhuanghong@sina.com>
2017-02-07 23:36:41 +0800

    配置更多的日志配置项进行验证

--- simple-logback-demo/src/main/java/com/steven/logback/demo/app/SimpleLogbackDemo.java
+++ simple-logback-demo/src/main/java/com/steven/logback/demo/app/SimpleLogbackDemo.java
@@ -3,6 +3,11 @@ package com.steven.logback.demo.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * 日志大小配置测试，分别基于1.1.7和1.1.8版本进行验证
+ * @author liuzhuanghong
+ *
+ */
 public class SimpleLogbackDemo {
 	// 定义一个全局的记录器，通过LoggerFactory获取
 	private final static Logger logger = LoggerFactory.getLogger(SimpleLogbackDemo.class);
