2f4579711390fb744374b45fc1c7f02b7e2c2bfe
liuzhuanghong <liuzhuanghong@sina.com>
2017-04-04 19:43:16 +0800

    add getVersion

--- simple-springboot-web-demo/src/main/java/com/steven/springboot/demo/app/SimpleWebApplication.java
+++ simple-springboot-web-demo/src/main/java/com/steven/springboot/demo/app/SimpleWebApplication.java
@@ -15,6 +15,12 @@ public class SimpleWebApplication {
 		return "Index of Simple Web Demo";
 	}
 
+	@RequestMapping(value = "/version")
+	public String getVersion() {
+		System.out.println("Version:0.0.1");
+		return "0.0.1";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleWebApplication.class, args);
 	}
